9041458af4066866a87baf53445dd56af5c6f481
Mike Esposito <mikeesposito6852@gmail.com>
2022-03-02 17:46:45 -0800

    Add turn to Game

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_03_012419) do
+ActiveRecord::Schema.define(version: 2022_03_03_013645) do
 
   create_table "games", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "turn"
   end
 
 end

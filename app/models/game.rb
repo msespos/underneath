9b29fa8732b372d03ed8032c0056563eb79673cf
Mike Esposito <mikeesposito6852@gmail.com>
2022-03-03 15:02:32 -0800

    Add dependent: :destroy to three associations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,5 @@
 class Game < ApplicationRecord
-  has_one :worm_piece
-  has_many :humans
-  has_many :cards
+  has_one :worm_piece, dependent: :destroy
+  has_many :humans, dependent: :destroy
+  has_many :cards, dependent: :destroy
 end

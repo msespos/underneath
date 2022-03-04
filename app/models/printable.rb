5312c413be6e10ae00a61454051c7d9ff27c0a2c
Mike Esposito <mikeesposito6852@gmail.com>
2022-03-04 08:20:44 -0800

    Add print_state method for printing the game state in the console (placement of cards, humans and worm piece)

--- app/models/printable.rb
+++ app/models/printable.rb
@@ -1,2 +1,23 @@
 module Printable
+  def print_state
+    board = Array.new(8) { Array.new(8) { '-' } }
+    board.each_with_index do |column, i|
+      column.each_with_index do |row, j|
+        humans.each do |h|
+          if h.x_position == i and h.y_position == j
+            board[j][i] = 'H'
+          end
+        end
+        cards.each do |c|
+          if c.x_position == i and c.y_position == j
+            board[j][i] = 'C'
+          end
+        end
+        if worm_piece.x_position == i and worm_piece.y_position == j
+          board[j][i] = 'W'
+        end
+      end
+    end
+    puts board.reverse.map { |e| e.join(' ') }
+  end
 end

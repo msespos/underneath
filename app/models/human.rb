aa2f82f6536a5b6746a1bdeecab30e00a43f42e3
Mike Esposito <mikeesposito6852@gmail.com>
2022-03-04 15:34:38 -0800

    Add Human#on_board?

--- app/models/human.rb
+++ app/models/human.rb
@@ -5,4 +5,8 @@ class Human < ApplicationRecord
     self.x_position += x
     self.y_position += y
   end
+
+  def on_board?
+    (0..7).include?(x_position) && (0..7).include?(y_position)
+  end
 end

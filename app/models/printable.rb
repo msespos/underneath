7ce98b3001e051cbbd909298e39c1a181f2fe07d
Mike Esposito <mikeesposito6852@gmail.com>
2022-03-14 13:21:45 -0700

    Refactor print_state and place_item to remove unnecessary loops and repetition

--- app/models/printable.rb
+++ app/models/printable.rb
@@ -1,17 +1,9 @@
 module Printable
   def print_state
     board = Array.new(Game::BOARD_SIZE) { Array.new(Game::BOARD_SIZE) { '-' } }
-    board.each_with_index do |row, y|
-      row.each_with_index do |column, x|
-        (humans + cards + [worm]).each do |item|
-          place_item(board, item, x, y)
-        end
-      end
+    (humans + cards + [worm]).each do |item|
+      board[item.y_position][item.x_position] = item.print_label
     end
     puts board.reverse.map { |e| e.join(' ') }
-  end
-
-  def place_item(board, item, x, y)
-    board[y][x] = item.print_label if item.x_position == x and item.y_position == y
   end
 end

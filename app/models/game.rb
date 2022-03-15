92398331f0b47530574554c466a8f341a114cbc3
Mike Esposito <mikeesposito6852@gmail.com>
2022-03-15 08:34:36 -0700

    Remove existing move methods to be placed in Human, Worm and Piece classes

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,27 +5,4 @@ class Game < ApplicationRecord
   include Printable
 
   BOARD_SIZE = 8.freeze
-
-  def valid_human_move?(start, finish)
-    # starts on board
-    # finishes on board
-    # start space has human, it is the humans' turn, and
-      # finish space is a human move away and is not occupied by a human
-  end
-
-  def valid_worm_move?(start, finish)
-    # starts on board
-    # finishes on board
-    # start space has worm, it is the worm's turn,
-      # and finish space is a worm move away and is not a rock
-  end
-
-  def on_board?(square)
-  end
-
-  def human_move_away?(start, finish)
-  end
-
-  def worm_move_away?(start, finish)
-  end
 end

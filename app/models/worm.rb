faae9914770a72ee0b25602ef8695258ab18e26a
Mike Esposito <mikeesposito6852@gmail.com>
2022-03-15 09:18:43 -0700

    Add one_move_away? to Worm

--- app/models/worm.rb
+++ app/models/worm.rb
@@ -4,4 +4,11 @@ class Worm < Piece
   def print_label
     'W'
   end
+
+  def one_move_away?(a, b)
+    (x_distance(a, b) == 1 || x_distance(a, b) == 2) && y_distance(a, b) == 0 ||
+    x_distance(a, b) == 0 && (y_distance(a, b) == 1 || y_distance(a, b) == 2) ||
+    x_distance(a, b) == 1 && y_distance(a, b) == 1 ||
+    x_distance(a, b) == 2 && y_distance(a, b) == 2
+  end
 end

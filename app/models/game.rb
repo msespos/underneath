a44486f69f0aa786cfef3d82975faf87c44261ea
Mike Esposito <mikeesposito6852@gmail.com>
2022-03-19 16:39:27 -0700

    Debug set_up_humans, set_up_worm and reset functions

--- app/models/game.rb
+++ app/models/game.rb
@@ -14,21 +14,22 @@ class Game < ApplicationRecord
 
   def set_up_humans
     [[1, 0, 1], [2, 0, 3], [3, 1, 0], [4, 3, 0]].each do |p|
-      Human.create!({ alive: true, x_position: p[1], y_position: p[2],
+      humans.create({ alive: true, x_position: p[1], y_position: p[2],
                        play_order: p[0], game_id: self.id })
     end
   end
 
   def set_up_worm
-    Worm.create!({ alive: true, x_position: 7, y_position: 7, game_id: self.id })
+    create_worm({ alive: true, x_position: 7, y_position: 7, game_id: self.id })
   end
 
   def set_up_cards
   end
 
   def reset
-    turn = 1
-    phase = "human 1"
+    self.turn = 1
+    self.phase = "human 1"
     (humans + cards + [worm]).map(&:destroy)
+    self.save
   end
 end

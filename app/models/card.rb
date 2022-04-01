101ffd6829ced121eff061c170537b9002ed9610
Mike Esposito <mikeesposito6852@gmail.com>
2022-04-01 13:25:31 -0700

    Add card reveal method and update print_state to allow nil values

--- app/models/card.rb
+++ app/models/card.rb
@@ -4,4 +4,23 @@ class Card < ApplicationRecord
   def print_label
     'C'
   end
+
+  def reveal
+    # ADD LAST REVEALED CARD MESSAGE FIELD TO GAME
+    if type == 'Rock'
+      self.face_up = true
+      game.last_revealed_card_message = 'Rock revealed'
+    elsif type == 'Bomb'
+      game.humans_bombs += 1
+      self.x_position = nil
+      self.y_position = nil
+      game.last_revealed_card_message = 'Bomb added to inventory'
+    else
+      self.x_position = nil
+      self.y_position = nil
+      game.last_revealed_card_message = 'Blank revealed'
+    end
+    self.save
+    game.save
+  end
 end
--- app/models/printable.rb
+++ app/models/printable.rb
@@ -2,7 +2,9 @@ module Printable
   def print_state
     board = Array.new(Game::BOARD_SIZE) { Array.new(Game::BOARD_SIZE) { '-' } }
     all_pieces.each do |item|
-      board[item.y_position][item.x_position] = item.print_label
+      unless item.x_position == nil || item.y_position == nil
+        board[item.y_position][item.x_position] = item.print_label
+      end
     end
     puts board.map { |e| e.join(' ') }
   end

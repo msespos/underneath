79cd59736b7c8e54ae1a4c4770c702b6ed603686
Mike Esposito <mikeesposito6852@gmail.com>
2022-04-06 18:24:22 -0700

    Remove valid_bomb_placement? condition from place_bomb and format logic statements to be more clear and add target_square_not_a_card?

--- app/models/human.rb
+++ app/models/human.rb
@@ -6,34 +6,48 @@ class Human < Piece
   end
 
   def place_bomb(v)
-    if valid_bomb_placement?(v)
-      ActiveBomb.create({ x_position: x_position + v[0],
-                          y_position: y_position + v[1],
-                          game_id: game.id })
-      game.humans_bombs -= 1
-    end
+    ActiveBomb.create({ x_position: x_position + v[0],
+                        y_position: y_position + v[1],
+                        game_id: game.id })
+    game.humans_bombs -= 1
   end
 
   def valid_bomb_placement?(v)
-    game.humans_bombs > 0 && valid_move?(v)
+    game.humans_bombs > 0 &&
+    valid_move?(v) &&
+    target_square_not_a_card?(v)
   end
 
   def valid_move?(v)
-    valid_move_geometry?(v) && target_square_not_a_human?(v) && target_square_not_an_active_bomb?(v)
+    valid_move_geometry?(v) &&
+    target_square_not_a_human?(v) &&
+    target_square_not_an_active_bomb?(v)
   end
 
   def valid_move_geometry?(v)
-    start_and_finish_on_board?(v) && kings_move?(v) && number_of_king_moves_away(v) == 1
+    start_and_finish_on_board?(v) &&
+    kings_move?(v) &&
+    number_of_king_moves_away(v) == 1
   end
 
   def kings_move?(v)
-    v[0] == 0 && [-1, 1].include?(v[1]) || v[1] == 0 && [-1, 1].include?(v[0]) ||
+    v[0] == 0 && [-1, 1].include?(v[1]) ||
+    v[1] == 0 && [-1, 1].include?(v[0]) ||
     [-1, 1].include?(v[0]) && v[0].abs == v[1].abs
   end
 
   def target_square_not_a_human?(v)
     game.humans.each do |h|
       if x_position + v[0] == h.x_position && y_position + v[1] == h.y_position
+        return false
+      end
+    end
+    true
+  end
+
+  def target_square_not_a_card?(v)
+    game.cards.each do |c|
+      if x_position + v[0] == c.x_position && y_position + v[1] == c.y_position
         return false
       end
     end

5817e4a50d328c9a42d2326c5468d833e9a6f678
Mike Esposito <mikeesposito6852@gmail.com>
2022-04-16 17:41:13 -0700

    Format create hashes for more clarity

--- app/models/game.rb
+++ app/models/game.rb
@@ -31,13 +31,18 @@ class Game < ApplicationRecord
 
   def set_up_humans
     [[1, 0, 1], [2, 0, 3], [3, 1, 0], [4, 3, 0]].each do |p|
-      humans.create({ alive: true, x_position: p[1], y_position: p[2],
-                      play_order: p[0], game_id: self.id })
+      humans.create({ alive: true,
+                      play_order: p[0],
+                      x_position: p[1],
+                      y_position: p[2],
+                      game_id: self.id })
     end
   end
 
   def set_up_worm
-    create_worm({ alive: true, x_position: 7, y_position: 7,
+    create_worm({ alive: true,
+                  x_position: 7,
+                  y_position: 7,
                   game_id: self.id })
   end
 

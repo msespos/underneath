e59a9500da894b898d59195fc0c442c8b5540ef3
Mike Esposito <mikeesposito6852@gmail.com>
2022-05-25 14:49:38 -0700

    Add comments

--- app/models/card.rb
+++ app/models/card.rb
@@ -3,6 +3,7 @@ require 'matrix'
 class Card < ApplicationRecord
   belongs_to :game
 
+  # not tested; just printing
   def print_label(view = :humans)
     if type == 'Rock' && (self.face_up == true && !human_on_card? || view == :worm)
       'R'
@@ -13,10 +14,12 @@ class Card < ApplicationRecord
     end
   end
 
+  # tested
   def human_on_card?
     game.humans.detect { |h| h.x_position == x_position && h.y_position == y_position }
   end
 
+  # not tested; just writing data
   def reveal
     if type == 'Rock'
       self.face_up = true

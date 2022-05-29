9045a880f1669cc017add8fdeac2dd70b8f9251f
Mike Esposito <mikeesposito6852@gmail.com>
2022-05-28 20:59:38 -0700

    Fix bug where human would not die when moving onto worm

--- app/models/game.rb
+++ app/models/game.rb
@@ -111,7 +111,7 @@ class Game < ApplicationRecord
     if type == 'move'
       move(v)
       item_on_square(cards, active_piece.position)&.reveal unless phase == 'worm'
-      item_on_square(humans, worm.position)&.die!
+      item_on_square(humans.reload, worm.reload.position)&.die!
     elsif type == 'place bomb'
       place_bomb(v)
       bomb_emergent_worm(v)
